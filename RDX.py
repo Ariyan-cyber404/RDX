bf05b8c623c572a6715c2f0491c25a068d35f597
Ariyan-404 <99079519+Ariyan-cyber404@users.noreply.github.com>
2023-07-26 00:58:08 +0600

    Add files via upload

--- RDX.py
+++ RDX.py
@@ -1,11 +1,11 @@
 #<\>!python3.11
 import os,platform,time
-import rdx64 as rdx
+import rdx64 as RDX
 fuck=platform.architecture()[0]
 if fuck=="64bit":
     os.system('clear');print('[!] Your Device is 64 bit');time.sleep(1);print('\n\n[!] Your Python Version :');time.sleep(1);os.system('python --version')
     time.sleep(2)
-    import rdx.register_device
+    import RDX.register_device()
 else:
     print('\nYOUR DEVICE 32 BIT NOT SUPPORT')
 
