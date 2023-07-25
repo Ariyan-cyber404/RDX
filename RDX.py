17d4235f1ed2557bab8cfdfc6f0b704778cd48eb
Ariyan-404 <99079519+Ariyan-cyber404@users.noreply.github.com>
2023-07-26 00:53:42 +0600

    Add files via upload

---
+++ RDX.py
@@ -0,0 +1,14 @@
+#<\>!python3.11
+import os,platform,time
+fuck=platform.architecture()[0]
+if fuck=="64bit":
+    os.system('clear');print('[!] Your Device is 64 bit');time.sleep(1);print('\n\n[!] Your Python Version :');time.sleep(1);os.system('python --version')
+    time.sleep(2)
+    import rdx64.register_device
+else:
+    print('\nYOUR DEVICE 32 BIT NOT SUPPORT')
+
+#---------------------------------------------------------#
+#                    THIS TOOL OWNED BY
+#                  • MAHADI HASAN AFRIDI •
+#---------------------------------------------------------#
